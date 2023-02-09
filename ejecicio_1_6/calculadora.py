dc1487f9b4461acc477a53151a6dcf1153281d79
Roman-Dario-Esquivel <Roman-Dario-Esquivel>
2023-02-09 00:32:26 -0300

    ejecitacion python2

---
+++ ejecicio_1_6/calculadora.py
@@ -0,0 +1,14 @@
+def suma(a,b):
+    return a+b
+def resta(a,b):
+    return a-b
+def multiplicacion(a,b):
+    return a*b 
+def divicion(a,b):
+    return a/b 
+def resto_div_enteros(a,b):
+    return a%b 
+def divicion_entera(a,b):
+    return (a-resto_div_enteros(a,b))/b
+
+print(__name__)
---
+++ ejecicio_1_6/main.py
@@ -0,0 +1,6 @@
+import calculadora
+
+
+print(__name__)
+res = calculadora.divicion_entera(21,5)
+print(res)
